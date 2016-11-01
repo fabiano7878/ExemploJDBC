f922872b40416cb8ea8f6804130d45d5171670fe
Fabiano dos Santos Silva <fabiano.asistemas@gmail.com>
2016-11-01 11:02:05 -0200

    Add files via upload
    ExemploJDBC

---
+++ JDBC1/ConnectionFactory.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	
+	public Connection getConnection(){		
+		try{
+	return DriverManager.getConnection("jdbc:microsoft:sqlserver://labs007:1433;User=internet;Password=ntrnt");
+		}catch(SQLException ex){
+		throw new RuntimeException();
+		}
+	}
+
+}
